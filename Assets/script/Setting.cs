804fefeea3efe8cb82a471f8d833ddaf812733b0
Unknown <q65310875@gmail.com>
2017-11-15 19:23:14 +0800

    add main memu, Press Space to select menu

---
+++ Assets/script/Setting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.script
+{
+    
+
+    public static class Setting
+    {
+        public static class Scene
+        {
+            public const string Start = "Assets/scene/Start.unity";
+        }
+    }
+}
---
+++ Assets/script/StartScene/EventChecker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Assets.script;
+
+public class EventChecker : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(Setting.Scene.Start, LoadSceneMode.Single);
+        }
+    }
+}
