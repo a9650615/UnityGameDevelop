30b1312e9a0b7e5a28404785d24f708ede18a71d
菜鳥祐 <a9650615@gmail.com>
2017-11-28 21:14:59 +0800

    change asset path & simple move & shoot system

--- Assets/script/Game/PlayerControl.cs
+++ Assets/script/Game/PlayerControl.cs
@@ -3,11 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
-
+public class PlayerControl : MonoBehaviour 
+{
+    public GameObject _bullet;
+    public float _moveSpeed = 10.5f;
+    private Vector2 direction;
+    private GameObject _player;
 	// Use this for initialization
 	void Start () {
-		
+        _player = GameObject.Find("Player");
 	}
 
     // 該方法接收 a 點與 b 點, a 點為物件位置, b 點為目標位置
@@ -62,10 +66,12 @@ public class PlayerControl : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        //Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        var mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
         // get direction you want to point at
-        Vector2 direction = (mouseScreenPosition).normalized;
+        direction = (mousePos).normalized;
 
         // set vector of transform directly
         GameObject.Find("Player").GetComponent<Transform>().up = direction;
@@ -73,5 +79,48 @@ public class PlayerControl : MonoBehaviour {
         screenCenter.x = Screen.width / 2;
         screenCenter.y = Screen.height / 2;
         //Debug.Log(GetDirection(GetAngle(screenCenter, (Vector2)Input.mousePosition)));
+
+        CheckIsClick();
+        MoveCheck();
+        CameraMove();
+    }
+
+    void CheckIsClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+			GameObject childGameObject = Instantiate(_bullet);
+            childGameObject.SetActive(true);
+            childGameObject.GetComponent<Transform>().position = GameObject.Find("Weapon").transform.position;
+            childGameObject.GetComponent<Transform>().up = GameObject.Find("Player").GetComponent<Transform>().up;
+            childGameObject.GetComponent<Transform>().Rotate(new Vector3(0f, 0f, 90f));
+            childGameObject.GetComponent<Rigidbody2D>().AddForce(GameObject.Find("Player").GetComponent<Transform>().up * 0.05f);
+        }
+    }
+
+    void MoveCheck()
+    {
+        Rigidbody2D move = _player.GetComponent<Rigidbody2D>();;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            move.AddForce(Vector2.up * _moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            move.AddForce(Vector2.left * _moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            move.AddForce(Vector2.down * _moveSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            move.AddForce(Vector2.right * _moveSpeed);
+        }
+    }
+
+    void CameraMove()
+    {
+        GameObject.Find("Main Camera").GetComponent<Transform>().position = new Vector3(_player.transform.position.x, _player.transform.position.y, -10);
     }
 }
