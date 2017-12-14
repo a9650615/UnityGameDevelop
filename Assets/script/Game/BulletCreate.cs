c1d78f0e8354ab4cea9dbf4f52f34efee89886e5
DESKTOP-B5OVVH6\Yu Hung <a0201751@yahoo.com.hk>
2017-12-14 17:16:31 +0800

    bullet

---
+++ Assets/script/Game/BulletCreate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCreate : MonoBehaviour {
+
+    public GameObject Bullet;
+    private GameObject BulletClone;
+
+    public float time = 0;
+    private float BulletInterval;
+
+
+	// Use this for initialization
+	void Start () {
+        BulletInterval = 0.5f;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && time > BulletInterval)////(2)
+        {
+            Vector3 pos = transform.position;
+            Quaternion rot = transform.rotation;
+            BulletClone = (GameObject)Instantiate(Bullet, pos, rot);////(3)
+            BulletClone.AddComponent("BulletState");////(4)
+            time = 0;
+        }
+    }
+}
+
