73cd13df19278b5fed1d1f53832993e3fd35b481
菜鳥祐 <a9650615@gmail.com>
2017-12-28 03:12:01 +0800

    怪物砲塔破壞功能

--- Assets/script/Entity/Monster.cs
+++ Assets/script/Entity/Monster.cs
@@ -5,7 +5,9 @@ public class Monster : CanBeAttack
 {
     public float speed = 3.0f;
     public float viewRange = 10f;
-	public float updateTargetTime = 3f;
+	public float updateTargetTime = 4f;
+    public float attackTime = 1f;
+    public float attackRange = 0.5f;
 
     private Vector2 oldPosition;
     private float nowTargetTime = 0;
@@ -31,18 +33,22 @@ public class Monster : CanBeAttack
         gameObject.tag = "Monster";
     }
 
+
 	// Update is called once per frame
 	void Update()
 	{
         bool stopMove = false;
+        bool isAttack = false;
         nowTargetTime += Time.deltaTime;
-        Collider2D[] hitColliders = {};
 		int i = 0;
+        Collider2D[] hitColliders = {};
+        hitColliders = Physics2D.OverlapCircleAll(transform.position, viewRange);
+        oldPosition = transform.position;
 
-        if (nowTargetTime > updateTargetTime)
+        if (nowTargetTime > attackTime)
         {
-            hitColliders = Physics2D.OverlapCircleAll(transform.position, viewRange);
-            oldPosition = transform.position;
+            isAttack = true;
+            nowTargetTime = 0;
         }
 
         while (i < hitColliders.Length && !stopMove)
@@ -53,8 +59,12 @@ public class Monster : CanBeAttack
                 //Debug.Log(hitColliders[i].tag);
                 if (hitColliders[i].tag == "CanCrack" || hitColliders[i].tag == "Player")
                 {
-					float step = speed * Time.deltaTime;
-					gameObject.transform.position = Vector2.MoveTowards(oldPosition, hitColliders[i].transform.position, step);
+                    if (isAttack&&hitColliders[i].tag == "CanCrack" && Vector3.Distance(gameObject.transform.position, hitColliders[i].transform.position) < attackRange)
+					{
+                        hitColliders[i].GetComponent<CanBeAttack>().BeAttack(5);
+					}
+                    float step = speed * Time.deltaTime;
+                    gameObject.transform.position = Vector2.MoveTowards(oldPosition, hitColliders[i].transform.position, step);
 				}
             }
             i++;
