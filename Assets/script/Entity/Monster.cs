cd0c52846e7308dd608286051d65be14463c3675
菜鳥祐 <a9650615@gmail.com>
2017-12-29 03:07:47 +0800

    提高怪物智商  不會跟金魚一樣

--- Assets/script/Entity/Monster.cs
+++ Assets/script/Entity/Monster.cs
@@ -7,11 +7,15 @@ public class Monster : CanBeAttack
     public float viewRange = 10f;
 	public float updateTargetTime = 4f;
     public float attackTime = 1f;
+    public float searchTargetTime = 3f;
+    private float searchTime = 1f;
     public float attackRange = 0.5f;
     public int attack = 10;
 
     private Vector2 oldPosition;
     private float nowTargetTime = 0;
+    private GameObject target;
+    private bool isSearch = false;
 
     public override string GetName()
     {
@@ -41,15 +45,23 @@ public class Monster : CanBeAttack
         bool stopMove = false;
         bool isAttack = false;
         nowTargetTime += Time.deltaTime;
+        searchTime += Time.deltaTime;
 		int i = 0;
         Collider2D[] hitColliders = {};
         hitColliders = Physics2D.OverlapCircleAll(transform.position, viewRange);
         oldPosition = transform.position;
-
+        isSearch = false;
         if (nowTargetTime > attackTime)
         {
             isAttack = true;
             nowTargetTime = 0;
+        }
+
+        if (searchTime > searchTargetTime || target == null)
+        {
+            isSearch = true;
+            searchTime = 0;
+            Debug.Log("research");
         }
 
         while (i < hitColliders.Length && !stopMove)
@@ -64,11 +76,17 @@ public class Monster : CanBeAttack
                     {
                         hitColliders[i].GetComponent<CanBeAttack>().BeAttack(attack);
 					}
-                    float step = speed * Time.deltaTime;
-                    gameObject.transform.position = Vector2.MoveTowards(oldPosition, hitColliders[i].transform.position, step);
-				}
+                    if (isSearch)
+                    {
+						target = hitColliders[i].gameObject;
+						stopMove = true;
+                    }
+                }
             }
             i++;
         }
+		float step = speed * Time.deltaTime;
+        if (target)
+        gameObject.transform.position = Vector2.MoveTowards(oldPosition, target.transform.position, step);
 	}
 }
